1881bc899b2c15e7b91c2f677bb0a7dc1ec45ccd
Godeta <Godeta61@gmail.com>
2020-05-21 16:07:41 +0200

    Ajout d'une librairie et un plug-in Eclipse (infos dans readme dans lib) + color test pour tester l'affichage des couleurs dans la ocnsole

--- src/gobblets/ColorTest.java
+++ src/gobblets/ColorTest.java
@@ -1,10 +1,26 @@
 package gobblets;
 
+//librairie pour utiliser les couleurs
+import org.fusesource.jansi.AnsiConsole;
+import static org.fusesource.jansi.Ansi.*;
+import static org.fusesource.jansi.Ansi.Color.*;
+
 public class ColorTest {
 
 	public static void main(String[] args) {
-		// Une classe pour tester l'affichage en couleur dans la console !
-		System.out.println ((char)27 + "[2J");
+		//pour utiliser jansi avec le plug-in Eclipse
+		System.setProperty("jansi.passthrough", "true");
+		
+		AnsiConsole.systemInstall();
+		/*test pour vérifier que le plug-in eclipse http://mihai-nita.net/2013/06/03/eclipse-plugin-ansi-in-console/
+		 *  a bien été installé et permet de voir les couleurs
+		 */
+		System.out.println("Hello \u001b[1;31mred\u001b[0m world!");
+		
+		// Une classe pour tester l'affichage en couleur dans la console avec jansi !
+		System.out.println( ansi().eraseScreen().fg(BLUE).a("Hello").fg(GREEN).a(" World").reset() );
 	}
+
+
 
 }
